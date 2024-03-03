ac871f77f5d1f85fa5396f0b75a5ab107f6a3b49
Your Name <you@example.com>
2024-03-03 16:07:22 -0500

    Work in progress, fetched pet data

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ const dynamicDate = currentDate;
 
 const footerText = document.querySelector(".text-info");
 footerText.textContent = `© ${dynamicDate} - TekSkillDev Pet Adoption Center`;
+
+async function petsArea() {
+  const petsPromise = await fetch(
+    "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
+  );
+  const petsData = await petsPromise.json();
+  petsData.forEach((pet) => {
+    console.log(pet.name);
+  });
+}
+petsArea();
