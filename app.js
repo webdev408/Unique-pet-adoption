f6194781bc0f0fa0e0d127b75f53a57e808c055c
Your Name <you@example.com>
2024-03-04 16:56:49 -0500

    Ignore system files

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const template = document.querySelector("#pet-card-template");
 const wrapper = document.createDocumentFragment();
 
-const currentDate = new Date().getFullYear();
-const dynamicDate = currentDate;
-
-const footerText = document.querySelector(".text-info");
-footerText.textContent = `© ${dynamicDate} - TekSkillDev Pet Adoption Center`;
-
 async function petsArea() {
   const petsPromise = await fetch(
     "https://learnwebcode.github.io/bootcamp-pet-data/pets.json"
